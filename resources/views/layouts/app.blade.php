e57641a3284d7d8692425fb14ff90028ea6ba1c3
almiravenia <veniaalmira2018@gmail.com>
2023-10-07 16:33:53 +0700

    hosting vps laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,5 +32,6 @@
                 {{ $slot }}
             </main>
         </div>
+        <script src="https://cdn.tilwindcss.com/?plugins-forms"></script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Brand;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -29,8 +31,26 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Route untuk CRUD Brand
-Route::get('/brands', [BrandController::class, 'index'])->name('brand');
-Route::post('/brands', [BrandController::class, 'store'])->name('brand.store');
+//Route untuk CRUD Brand
+Route::middleware(['auth', 'verified'])->group(function() {
+    // menampilkan data
+    Route::get('/brands', [BrandController::class, 'index'])->name('brand.index');
+
+    // menampilkan form tambah data
+    Route::get('/brands/create', [BrandController::class, 'create'])->name('brand.create');
+
+    // menambahkan data ke database
+    Route::post('/brands', [BrandController::class, 'store'])->name('brand.store');
+
+    // menampilkan form edit data
+    Route::get('/brands/{id}/edit', [BrandController::class, 'edit'])->name('brand.edit');
+
+    //mengupdate data ke database
+    Route::put('/brands/{id}', [BrandController::class, 'update'])->name('brand.update');
+
+    // menghapus data dari database
+    Route::delete('/brands/{id}', [BrandController::class, 'destroy'])->name('brand.destroy');
+});
+
 
 require __DIR__.'/auth.php';
